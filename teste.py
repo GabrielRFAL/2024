4ff8d733054ebc09cca9e1d281549d89cb50dd78
Gabriel Ribas Ferreira de Almeida <165100529+GabrielRFAL@users.noreply.github.com>
2024-10-18 19:58:25 +0000

    teste de commit

---
+++ teste.py
@@ -0,0 +1,8 @@
+print("Teste de commit")
+
+"""
+Passo a passo de commit:
+1º git add . -> adicionar os arquivos no repositório
+2º git commit -m "mensagem do commit" -> para dar o commit
+3º git push -> para subir o código para o github
+"""
